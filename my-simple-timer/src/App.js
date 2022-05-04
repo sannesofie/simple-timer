7b3fcebdd46b3f7316b64a4d592f6f103d934a37
Sanne Sofie <sannesofie@me.com>
2022-05-04 16:26:12 +0200

    adding components

--- my-simple-timer/src/App.js
+++ my-simple-timer/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+/* eslint-disable no-undef */
 import './App.css';
+import { Header } from './components/header/Header';
+import { Button } from './components/button/Button';
+import { Timer } from './components/timer/Timer';
+
 
 function App() {
+  const [timer, setTime] = useState(Time)
+
+  const countDown = () => {
+    setInterval(setTime, 1000, (x) => x - 1);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Timer sec={timer} />
+      <Button handle={countDown} name="start" color="red" />
     </div>
   );
 }
