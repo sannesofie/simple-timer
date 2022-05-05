af2d4e18147db47c55ff2a8126b52d2962977d81
Sanne Sofie <sannesofie@me.com>
2022-05-05 14:25:43 +0200

    timer-works

--- my-simple-timer/src/App.js
+++ my-simple-timer/src/App.js
@@ -1,22 +1,23 @@
-/* eslint-disable no-undef */
+import { useState } from "react";
 import './App.css';
 import { Header } from './components/header/Header';
 import { Button } from './components/button/Button';
 import { Timer } from './components/timer/Timer';
+import { Time } from './data'
 
 
 function App() {
   const [timer, setTime] = useState(Time)
 
-  const countDown = () => {
+  const useCountDown = () => {
     setInterval(setTime, 1000, (x) => x - 1);
   };
 
   return (
     <div className="App">
       <Header />
+      <Button handle={useCountDown} name="Start"/>
       <Timer sec={timer} />
-      <Button handle={countDown} name="start" color="red" />
     </div>
   );
 }
